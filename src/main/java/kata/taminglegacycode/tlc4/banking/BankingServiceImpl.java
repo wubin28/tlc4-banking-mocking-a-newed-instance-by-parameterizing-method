852d0e36c774ed2907148b78f507e769a7e07a29
benwu <wubinben@icloud.com>
2019-08-25 18:49:11 +0800

    use @Spy and @InjectMocks to make test green

--- src/main/java/kata/taminglegacycode/tlc4/banking/BankingServiceImpl.java
+++ src/main/java/kata/taminglegacycode/tlc4/banking/BankingServiceImpl.java
@@ -4,11 +4,15 @@ public class BankingServiceImpl {
 
     public void exec(String id, JSONObject json, int length) {
         // ...
-        JsonListResult result = new JsonListResult(MessageCode.OK, SysConstants.ECHO_RESULT_OK, json.getString(SysConstants.JSON_NODE_NO));
+        JsonListResult result = buildResult(json);
         // ...
 
         if (length < 3) {
             result.setStatus(MessageCode.TSC_1);
         }
+    }
+
+    JsonListResult buildResult(JSONObject json) {
+        return new JsonListResult(MessageCode.OK, SysConstants.ECHO_RESULT_OK, json.getString(SysConstants.JSON_NODE_NO));
     }
 }
--- src/test/java/kata/taminglegacycode/tlc4/banking/BankingServiceImplTest.java
+++ src/test/java/kata/taminglegacycode/tlc4/banking/BankingServiceImplTest.java
@@ -1,9 +1,32 @@
 package kata.taminglegacycode.tlc4.banking;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BankingServiceImplTest {
+
+    @Spy
+    @InjectMocks
+    BankingServiceImpl bankingServiceImpl;
+
+    @Mock
+    JsonListResult jsonListResult;
+
     @Test
     public void status_of_result_should_be_set_to_TSC_1_if_length_is_less_than_3() {
+        doReturn(jsonListResult).when(bankingServiceImpl).buildResult(any(JSONObject.class));
+
+        // when
+        bankingServiceImpl.exec("id", new JSONObject(), 1);
+
+        // then
+        verify(jsonListResult, times(1)).setStatus(MessageCode.TSC_1);
     }
 }
