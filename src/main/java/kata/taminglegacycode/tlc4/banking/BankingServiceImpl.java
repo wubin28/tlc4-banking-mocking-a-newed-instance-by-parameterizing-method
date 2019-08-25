d7a9761d026104c649006130932a7464039136ae
benwu <wubinben@icloud.com>
2019-08-25 18:15:39 +0800

    extract instance method by spying or anonymous inner class by ZHONG Jing

--- src/main/java/kata/taminglegacycode/tlc4/banking/BankingServiceImpl.java
+++ src/main/java/kata/taminglegacycode/tlc4/banking/BankingServiceImpl.java
@@ -3,12 +3,36 @@ package kata.taminglegacycode.tlc4.banking;
 public class BankingServiceImpl {
 
     public void exec(String id, JSONObject json, int length) {
+        exec(id, json, length, new JsonListResult(MessageCode.OK, SysConstants.ECHO_RESULT_OK, json.getString(SysConstants.JSON_NODE_NO)));
+    }
+
+    public void exec(String id, JSONObject json, int length, JsonListResult result) {
         // ...
-        JsonListResult result = new JsonListResult(MessageCode.OK, SysConstants.ECHO_RESULT_OK, json.getString(SysConstants.JSON_NODE_NO));
         // ...
 
         if (length < 3) {
             result.setStatus(MessageCode.TSC_1);
+        }
+
+
+
+    }
+
+    @Test
+    void aaaTest() {
+        BankingServiceImpl spyService = spy(new BankingServiceImpl());
+
+        doReturn(mockResult).when(spyService).buildResult();
+
+    }
+
+    @Test
+    void bbbTest() {
+        BankingServiceImpl fakeService = new BankingServiceImpl() {
+            @Override
+            public void buildResult() {
+
+            }
         }
     }
 }
--- src/test/java/kata/taminglegacycode/tlc4/banking/BankingServiceImplTest.java
+++ src/test/java/kata/taminglegacycode/tlc4/banking/BankingServiceImplTest.java
@@ -2,8 +2,21 @@ package kata.taminglegacycode.tlc4.banking;
 
 import org.junit.Test;
 
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
 public class BankingServiceImplTest {
     @Test
     public void status_of_result_should_be_set_to_TSC_1_if_length_is_less_than_3() {
+        // given
+        JsonListResult mockResult = mock(JsonListResult.class);
+        BankingServiceImpl bankingServiceImpl = new BankingServiceImpl();
+
+        // when
+        bankingServiceImpl.exec("id", new JSONObject(), 1, mockResult);
+
+        // then
+        then(mockResult).should(times(1)).setStatus(MessageCode.TSC_1);
     }
 }
