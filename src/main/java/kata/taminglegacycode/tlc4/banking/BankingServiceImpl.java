c1d669eb560c95a2157725bff2331c81419f9988
benwu <wubinben@icloud.com>
2019-08-25 18:20:57 +0800

    back to baseline the 3rd time

--- src/main/java/kata/taminglegacycode/tlc4/banking/BankingServiceImpl.java
+++ src/main/java/kata/taminglegacycode/tlc4/banking/BankingServiceImpl.java
@@ -3,11 +3,8 @@ package kata.taminglegacycode.tlc4.banking;
 public class BankingServiceImpl {
 
     public void exec(String id, JSONObject json, int length) {
-        exec(id, json, length, new JsonListResult(MessageCode.OK, SysConstants.ECHO_RESULT_OK, json.getString(SysConstants.JSON_NODE_NO)));
-    }
-
-    public void exec(String id, JSONObject json, int length, JsonListResult result) {
         // ...
+        JsonListResult result = new JsonListResult(MessageCode.OK, SysConstants.ECHO_RESULT_OK, json.getString(SysConstants.JSON_NODE_NO));
         // ...
 
         if (length < 3) {
